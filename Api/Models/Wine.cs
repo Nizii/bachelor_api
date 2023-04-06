bb11074abf26727015d3ac2fef4287d55b3baa84
Nizam <nizam.oezdemir@gmx.ch>
2023-04-06 12:23:16 +0200

    first commit after reconfig

---
+++ Api/Models/Wine.cs
@@ -0,0 +1,31 @@
+using MongoDB.Bson;
+
+namespace Api.Models
+{
+    public class Wine
+    {
+        private const string PHOTO_URL = "interactivemenu.azurewebsites.net/Imgs";
+        //private const string PHOTO_URL = "https://localhost:44322/Imgs";
+        public int _id { get; set; }
+        public string Name { get; set; }
+        public string Winetype { get; set; }
+        public string Origin { get; set; }
+        public string Grape { get; set; }
+        public decimal Alcohol { get; set; }
+        public decimal Openprice { get; set; }
+        public decimal Bottleprice { get; set; }
+        public int Selected { get; set; }
+        public string Charakter { get; set; }
+        public string MatchWith { get; set; }
+        public string ServingTemperature { get; set; }
+
+        public string StorageTime { get; set; }
+        public int Year { get; set; }
+        public string[] Match { get; set; }
+
+        public string[] Profile { get; set; }
+
+
+        public string Link { get { return PHOTO_URL + Name; } }
+    }
+}
